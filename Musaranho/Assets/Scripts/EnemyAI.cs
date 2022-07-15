b724e373fbbaa6c5a108cbb50a688cd158522b6f
asrhiphop <asrhiphop@gmail.com>
2022-07-16 00:20:29 +0100

    Enemy AI

--- Musaranho/Assets/Scripts/EnemyAI.cs
+++ Musaranho/Assets/Scripts/EnemyAI.cs
@@ -23,6 +23,7 @@ public class EnemyAI : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
+        Debug.Log(target.name);
     }
 
     private void Update() {
@@ -31,23 +32,32 @@ public class EnemyAI : MonoBehaviour
         isInChaseRange = Physics2D.OverlapCircle(transform.position, sh.chaseRadius, whatIsPlayer);
         isInAttackRange = Physics2D.OverlapCircle(transform.position, sh.attackRadius, whatIsPlayer);
 
+         Debug.Log(isInChaseRange + " " + sh.chaseRadius + isInAttackRange + " " + sh.attackRadius);
+
         dir = target.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         dir.Normalize();
         movement = dir;
 
         if (shouldRotate) {
-            anim.SetFloat("X", dir.x);
-            anim.SetFloat("Y", dir.y);
+            int dirx = (int) dir.x * 10;
+            int diry = (int) dir.y * 10;
+            anim.SetInteger("X", dirx);
+            anim.SetInteger("Y", diry);
         }
     }
 
     private void FixedUpdate() {
-        if (isInChaseRange && !isInAttackRange) MoveCharacter(movement);
-        if (isInAttackRange) {
-            rb.velocity = Vector2.zero;
-            sh.Shoot();
+        if (isInChaseRange && !isInAttackRange) {
+            //Debug.Log("It's far");
+            MoveCharacter(movement);
         }
+        else if (isInAttackRange) {
+            rb.velocity = Vector2.zero;
+            //Debug.Log("It's shooting " + target.transform.position);
+            sh.Shoot(dir);
+        }
+        //else Debug.Log("Nothing");
     }
 
     private void MoveCharacter(Vector2 dir) {
