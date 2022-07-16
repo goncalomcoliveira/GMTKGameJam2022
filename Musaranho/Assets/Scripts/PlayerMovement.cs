76c6c01dcce56605bb3643cc1da235b446b7966c
asrhiphop <asrhiphop@gmail.com>
2022-07-16 11:13:11 +0100

    Weapon and DiceOne Animation

--- Musaranho/Assets/Scripts/PlayerMovement.cs
+++ Musaranho/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Camera cam;
+    public Animator anim;
+    public Transform rotatePoint;
 
     private Vector2 moveDirection;
 
@@ -27,17 +29,40 @@ public class PlayerMovement : MonoBehaviour
         Move();
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
+        rotatePoint.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     void ProcessInputs(){
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        if (moveX < 0)
+        /*
+        if (moveX < 0) {
+            anim.SetBool("isRunning", true);
             sprite.localScale = new Vector3(-1f, 1f, 1f);
-        else if (moveX > 0)
+        }
+        else if (moveX > 0) {
+            
             sprite.localScale = new Vector3(1f, 1f, 1f);
+        }
+        */
+
+        int movX, movY;
+        if (moveX >= 0)
+            movX = Mathf.CeilToInt(moveX);
+        else
+            movX = Mathf.FloorToInt(moveX);
+
+        if (moveY >= 0)
+            movY = Mathf.CeilToInt(moveY);
+        else
+            movY = Mathf.FloorToInt(moveY);
+
+        anim.SetInteger("X", movX);
+        anim.SetInteger("Y", movY);
+
+        if (moveX == 0 && moveY == 0) anim.SetBool("isRunning", false);
+        else anim.SetBool("isRunning", true);
 
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
