2c6302f1423c0ac0f4be01a9a99b4e5c07f6f94d
asrhiphop <asrhiphop@gmail.com>
2022-07-17 16:10:08 +0100

    Bomba

--- Musaranho/Assets/Scripts/Bomb.cs
+++ Musaranho/Assets/Scripts/Bomb.cs
@@ -7,10 +7,15 @@ public class Bomb : MonoBehaviour
 	public int range;
 	public int damage;
 	public float countdown = 2f;
+	public float explodeCountdown = 0.35f;
 	private AudioSystem s;
+	public Animator anim;
+
+	private bool play = false;
 
     private void Awake()
     {
+		anim.SetBool("isExploding", false);
 		s = GameObject.FindWithTag("AudioManager").GetComponent<AudioSystem>();
 	}
 
@@ -21,15 +26,26 @@ public class Bomb : MonoBehaviour
 
 		if (countdown <= 0f)
 		{
+			if (!play) {
+				s.Play("croissant_boom");
+				anim.SetBool("isExploding", true);
+				play = true;
+			}
+
+			explodeCountdown -= Time.deltaTime;
 			EnemyLife[] list = (EnemyLife[]) Resources.FindObjectsOfTypeAll(typeof(EnemyLife));
+			
 
             foreach (EnemyLife go in list)
             {
-				s.Play("croissant_boom");
 				go.Explode(range, damage);
 			}
 			
-			Destroy(gameObject);
+			if (explodeCountdown <= 0f) {
+				Destroy(gameObject);
+				anim.SetBool("isExploding", false);
+				play = false;
+			}
 		}
 	}
 }
