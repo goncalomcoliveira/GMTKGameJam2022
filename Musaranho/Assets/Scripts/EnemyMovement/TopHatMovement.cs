166a66b980350aa9e3e44790f476d7858b56eda6
asrhiphop <asrhiphop@gmail.com>
2022-07-17 19:20:14 +0100

    TopHat Animation

--- Musaranho/Assets/Scripts/EnemyMovement/TopHatMovement.cs
+++ Musaranho/Assets/Scripts/EnemyMovement/TopHatMovement.cs
@@ -8,6 +8,7 @@ public class TopHatMovement : MonoBehaviour
     public EnemyShooting sh;
     public float waitingAttack = 1f;
     public float waitingMove = 1f;
+    public float waitingAnim = 1f;
 
     public bool shouldRotate;
     public LayerMask whatIsPlayer;
@@ -24,10 +25,14 @@ public class TopHatMovement : MonoBehaviour
 
     private float nextTimeToAttack = 0f;
     private float nextTimeToMove = 0f;
+    private float nextTimeToAnim = 0f;
+
+    private bool locker = false;
+    private bool down = false;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         nextTimeToMove = Time.time + waitingMove;
         target = GameObject.FindWithTag("Player").transform;
     }
@@ -36,6 +41,15 @@ public class TopHatMovement : MonoBehaviour
         isInChaseRange = Physics2D.OverlapCircle(transform.position, sh.chaseRadius, whatIsPlayer);
 
         isInAttackRange = Physics2D.OverlapCircle(transform.position, sh.attackRadius, whatIsPlayer);
+
+        if (down) {
+            if (Time.time >= nextTimeToAnim) {
+                anim.SetBool("Up",false);
+                anim.SetBool("Down",false);
+                down = false;
+            }
+        }
+
         if(isInAttackRange && Time.time >= nextTimeToAttack) {
             dir = target.position - transform.position;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
@@ -48,10 +62,27 @@ public class TopHatMovement : MonoBehaviour
 
         //anim.SetBool("isRunning", (isInChaseRange && !isInAttackRange));
 
-        rb.MovePosition(target.position);
+        if (!locker) {
+            anim.SetBool("Up",true);
+            anim.SetBool("Down",false);
+            locker = true;
+            nextTimeToAnim = Time.time + waitingAnim;
+        }
+        else {
+            if (Time.time >= nextTimeToAnim) {
+                anim.SetBool("Up",false);
+                anim.SetBool("Down",true);
+                
+                rb.MovePosition(target.position);
 
-        nextTimeToAttack = Time.time + waitingAttack;
-        nextTimeToMove = Time.time + waitingMove;
+                down = true;
 
+                nextTimeToAnim = Time.time + waitingAnim;
+                nextTimeToAttack = Time.time + waitingAttack;
+                nextTimeToMove = Time.time + waitingMove;
+
+                locker = false;
+            }
+        }        
     }
 }
