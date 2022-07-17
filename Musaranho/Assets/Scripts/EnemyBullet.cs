41eb95d8cc5347306b526b268df048d987665349
asrhiphop <asrhiphop@gmail.com>
2022-07-17 11:36:50 +0100

    Shield

--- Musaranho/Assets/Scripts/EnemyBullet.cs
+++ Musaranho/Assets/Scripts/EnemyBullet.cs
@@ -6,14 +6,19 @@ public class EnemyBullet : MonoBehaviour
 {
     Transform player;
     public float range;
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if(coll.transform.tag == "Player")
-        {
-            coll.gameObject.GetComponent<health>().TakeDamage(1);
-            coll.gameObject.GetComponent<Shooting>().ChangeFace();
+        {   
+            if (!coll.gameObject.GetComponent<health>().sr.enabled) {
+                coll.gameObject.GetComponent<health>().TakeDamage(1);
+                coll.gameObject.GetComponent<Shooting>().ChangeFace();
+                coll.gameObject.GetComponent<health>().sr.enabled = true;
+                coll.gameObject.GetComponent<health>().isInvincible = Time.time + coll.gameObject.GetComponent<health>().invincibilityTime;
+            }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }
     public bool Bounce(float range)
     {
@@ -27,8 +32,10 @@ public class EnemyBullet : MonoBehaviour
     }
 
     void Update()
-    {
+    {   
         player = GameObject.FindWithTag("Player").transform;
+        if (Time.time >= player.GetComponent<health>().isInvincible && player.GetComponent<health>().sr.enabled) player.GetComponent<health>().sr.enabled = false;
+        
         if (Mathf.Sqrt(Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y, 2)) > range)
         {
             Destroy(gameObject);
